3e9f18c8717205188bc42b95fb45ceef84efcc91
IndexSoft1 <159001833+IndexSoft1@users.noreply.github.com>
2024-10-18 20:24:53 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
 onload = () =>{
     document.body.classList.remove("container");
+    setTimeout(() => {
+        animatedText.classList.add('show');
+        typeWriter();
+    }, 2000);
+   
 };
+
+const text = "❤  Rukiye  ❤";
+const animatedText = document.getElementById('animatedText');
+let index = 0;
+
+function typeWriter() {
+    if (index < text.length) {
+        animatedText.innerHTML += text.charAt(index);
+        index++;
+        setTimeout(typeWriter, 350); // Adjust the speed of typing here (200ms)
+    }
+}
+
